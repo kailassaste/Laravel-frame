dcf80cfbf6f5b71796dcb353d7b1404d49cc2429
kailas <kailassaste70@gmail.com>
2025-01-09 14:33:06 +0530

    view and controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 <!-- <h1>Hello php laravel <a href ='about'>go to  about page </a></h1> -->
 
-<a href="{{route('about')}}">Home 1</a>
+<!-- <a href="{{route('about')}}">Home 1</a>
 <a href="{{route('about')}}">Home 2</a>
-<a href="{{route('about')}}">Home 3</a>
+<a href="{{route('about')}}">Home 3</a> -->
+
+<h1>view page</h1>
